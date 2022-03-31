abd0e7c769f2f013f66b82b90518ee6d69e0eb53
vitamin777 <rlaalsdn8@gmail.com>
2022-03-31 18:47:44 +0900

    history

---
+++ prepare/front/component/AppLayout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const AppLayout = ({ children }) => {
+	return
+
+};
+
+AppLayout.propTypes = {
+	children: PropTypes.node.isRequired,
+};
+
+export default AppLayout;
---
+++ prepare/front/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'; // Next 에서는 이 구문이 필요가 없다.
+
+const Home = () => {
+	return (
+			<div>Hello, Next</div>
+	)
+}
+
+export default Home;
