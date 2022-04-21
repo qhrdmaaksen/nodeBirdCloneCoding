8903b3615622d244b7693e2e4df11769011b3d0a
vitamin777 <rlaalsdn8@gmail.com>
2022-04-22 06:25:31 +0900

    express install & router address system setting, with uses postman post request check commit

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,25 +1,35 @@
-const http = require('http')
-const server = http.createServer((req, res)=>{
-	console.log("req.url : " + req.url + ", req.method : " + req.method)
-	if (req.method === 'GET') {
-		if (req.url === '/api/posts') {
+const express = require('express')
 
-		}
-	} else if (req.method === 'POST') {
-		if (req.url === '/api/post') {
+const app = express() // 호출 한번 해줘야함
 
-		}
-	} else if (req.method === 'DELETE') {
-		if (req.url === '/api/post') {
-
-		}
-	}
-	res.write('<h1>Hello Node!01</h1>') // html 을 넣을 수도있다.
-	res.write('Hello Node!02')
-	res.write('Hello Node!03')
-	res.write('Hello Node!04')
-	res.end('hello node05') // end 는 마지막에만 사용
+app.get('/', (req, res) => {
+	res.send('hello express') // end === send
 })
-server.listen(3000, ()=> { // localhost:3000
+app.get('/api', (req, res) => {
+	res.send('hello api') // end === send
+})
+app.get('/api/posts', (req, res) => { // router 들
+	res.json([ // data 는 보통 json 으로 표현,api 들은 json 으로 응답
+		{id: 1, content: 'hello01'},
+		{id: 2, content: 'hello02'},
+		{id: 3, content: 'hello03'},
+	])
+})
+app.post('/api/post', (req, res) => {
+	res.json({id: 1, content: 'hello'})
+})
+app.delete('/api/post', (req, res) => {
+	res.json({id: 1})
+})
+app.get('/api/posts', (req, res) => {
+
+})
+app.get('/api/posts', (req, res) => {
+
+})
+app.get('/api/posts', (req, res) => {
+
+})
+app.listen(3000, () => { // localhost:3000
 	console.log('서버 실행 중')
 })
