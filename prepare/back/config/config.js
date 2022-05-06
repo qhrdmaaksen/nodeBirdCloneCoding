ade4727e6bde3d95711c256554ddb663e6f68e75
vitamin777 <rlaalsdn8@gmail.com>
2022-05-06 16:39:25 +0900

    update: config file modify aplly

--- prepare/back/config/config.js
+++ prepare/back/config/config.js
@@ -22,9 +22,8 @@ module.exports = {
   "production": {
     "username": "nodeBirdDB",
     "password": process.env.DB_PASSWORD, // .env password setting
-    "database": "database_production",
+    "database": "react-nodeBird",
     "host": "127.0.0.1",
-    "port": "3306",
     "dialect": "mysql"
   }
 }
